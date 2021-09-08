90925b139bb182d1d14112a505de7246a37a5b40
Chris <chirssel@gmail.com>
2021-09-09 00:04:34 +0200

    Update JDA version

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.1")
 
-    implementation("net.dv8tion:JDA:4.3.0_277")
+    implementation("net.dv8tion:JDA:4.3.0_324")
     implementation("com.github.minndevelopment:jda-ktx:ea0a1b2")
 
     implementation("com.zaxxer:HikariCP:2.3.2")
